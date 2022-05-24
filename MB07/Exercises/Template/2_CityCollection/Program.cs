8264b16debf276f3fa16a1f97339a4ef93c102b0
Benjamin Peter <benjaminpeter@outlook.com>
2022-05-24 17:05:00 +0200

    Iteration Aufgaben 2

--- MB07/Exercises/Template/2_CityCollection/Program.cs
+++ MB07/Exercises/Template/2_CityCollection/Program.cs
@@ -3,27 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace _2_CityCollection {
-    public class CityCollection {
+    public class CityCollection : IEnumerable<string> { 
         private string[] cities = {"Bern", "Basel", "Zürich", "Rapperswil", "Genf"};
 
+        public IEnumerable<string> Reverse {
+            get
+            {
+                for (int i = cities.Length - 1; i >= 0; i--)
+                {
+                    yield return cities[i];
+                }
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < cities.Length; i++)
+            {
+                yield return cities[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
         // TODO: Members für Test-Code in Main()-Methode erstellen
+
     }
 
     class Program {
         static void Main(string[] args) {
             CityCollection myColl = new CityCollection();
 
-            // Ausgabe
-            //foreach (string s in myColl)
-            //{
-            //	Console.WriteLine(s);
-            //}
+            //Ausgabe
+            foreach (string s in myColl)
+            {
+                Console.WriteLine(s);
+            }
 
-            // Ausgabe in umgekehrter Reihenfolge 
-            //foreach (string s in myColl.Reverse)
-            //{
-            //	Console.WriteLine(s);
-            //}
+            //Ausgabe in umgekehrter Reihenfolge
+            foreach (string s in myColl.Reverse)
+            {
+                Console.WriteLine(s);
+            }
 
             Console.ReadKey();
         }
