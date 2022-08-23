06abc7fccbc57ce8cefe9e26972acaf6a06f9230
Benjamin Peter <benjaminpeter@outlook.com>
2022-08-23 17:02:47 +0200

    Serializer created

--- MB17/Serialization_Vorgabe/ZBW.ProgAdvII.Serialization.BusinessLogic/Exercises/1/ZbwSerializer.cs
+++ MB17/Serialization_Vorgabe/ZBW.ProgAdvII.Serialization.BusinessLogic/Exercises/1/ZbwSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace ZBW.ProgAdvII.Serialization.BusinessLogic.Exercises._1
 {
@@ -7,7 +8,21 @@ namespace ZBW.ProgAdvII.Serialization.BusinessLogic.Exercises._1
     {
         public void Serialize(Student student, Stream stream)
         {
-            throw new NotImplementedException();
+            byte[] buffer = new byte[student.FirstName.Length + 1];
+            buffer[0] = Convert.ToByte(student.FirstName.Length);
+            Encoding.ASCII.GetBytes(student.FirstName).CopyTo(buffer, 1);
+            stream.Write(buffer, 0, student.FirstName.Length + 1);
+
+            buffer = new byte[student.LastName.Length + 1];
+            buffer[0] = Convert.ToByte(student.LastName.Length);
+            Encoding.ASCII.GetBytes(student.LastName).CopyTo(buffer, 1);
+            stream.Write(buffer, student.FirstName.Length + 1, student.LastName.Length + 1);
+
+            buffer = new byte[3];
+            int daysSince = Convert.ToInt32(student.DateOfBirth);
+            BitConverter.GetBytes(daysSince).CopyTo(buffer, 0);
+            stream.Write(buffer, student.FirstName.Length + 1 + student.LastName.Length + 1, 3);
+            stream.Flush();
         }
 
         public Student Deserialize(Stream stream)
